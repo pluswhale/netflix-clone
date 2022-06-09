aeb86e92b06d6d6f73c8ba271639a7e900c45381
Egor Dultsev <listenmy@yandex.ru>
2022-06-09 12:15:31 +0300

    Initial Commit

---
+++ netflix-clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflix-clone
+//
+//  Created by Egor Dultsev on 09.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
